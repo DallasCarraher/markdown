9bdf6f8dc475b918644dc741f9dadc8e45f82df5
Dallas Carraher <dallas.carraher@gmail.com>
2020-09-01 17:52:42 -0700

    .markdown navigation

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="WELCOME TO MY PAGE" />
+    <Navigation />
+    <Notes />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Navigation from "./components/Navigation";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Navigation,
   },
 };
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,25 @@
+<template>
+  <header id="navigation">
+    <h2 id="my-name">.markdown</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Navigation",
+  components: {},
+};
+</script>
+
+<style scoped>
+#navigation {
+  display: flex;
+  flex-direction: row;
+  border-bottom: solid;
+  padding: 10px;
+  margin: 0;
+}
+#my-name {
+  font-weight: "bold";
+}
+</style>
