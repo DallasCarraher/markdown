9e0d891cc6f80613d2e29ec82bc0e4c649c8b328
Dallas Carraher <dallas.carraher@gmail.com>
2020-09-07 13:59:03 -0700

    ?

---
+++ tests/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-view />
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      documentId: 'uTixWrVf75KfC2D37iR8',
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
