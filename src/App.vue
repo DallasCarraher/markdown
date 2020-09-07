0b05f71d7af96c511742c4850ec4d65574b06498
Dallas Carraher <dallas.carraher@gmail.com>
2020-09-06 21:25:33 -0700

    getting rid of the route comments

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <router-view />
-
-  <!-- <div id="nav">
-    <router-link to="/">Home</router-link>|
-    <router-link to="/about">About</router-link>
-  </div>-->
 </template>
 
 <script>
