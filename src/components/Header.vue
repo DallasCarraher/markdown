42a2505e4232b9d4361fe8885e4a60ca1cd8b694
Dallas Carraher <dallas.carraher@gmail.com>
2020-09-07 23:09:15 -0700

    more header functionality

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,15 @@
 <template>
   <header id="header">
-    <h2 id="title">
-      <a href="/" @mouseover="title = '← go back'" @mouseout="title = '.markdown'">{{ title }}</a>
+    <h2
+      id="title"
+      @mouseover="previousPage && (headerTitle = `← ${previousPage}`)"
+      @mouseout="headerTitle = title"
+      @click="previousPage && navigate"
+    >
+      {{ headerTitle }}
+    </h2>
+    <h2 id="sign-out" @click="signOut">
+      sign out
     </h2>
   </header>
 </template>
@@ -10,10 +18,33 @@
 export default {
   name: 'Header',
   components: {},
+  props: {
+    title: String,
+    previous: String,
+  },
   data() {
     return {
-      title: '.markdown',
+      headerTitle: this.title,
+      previousPage: this.previous,
     };
+  },
+  methods: {
+    navigate() {
+      const userId = localStorage.getItem('uid');
+      console.log(userId);
+      if (userId) {
+        this.$router.push({
+          name: this.previousPage,
+          params: { userId },
+        });
+      }
+    },
+    signOut() {
+      localStorage.removeItem('uid');
+      this.$router.push({
+        name: 'Home',
+      });
+    },
   },
 };
 </script>
@@ -27,8 +58,15 @@ export default {
   margin: 0;
 }
 #title {
+  flex-direction: row;
+  flex: 12;
+  font-weight: 'bold';
+  cursor: pointer;
+}
+#sign-out {
   flex: 1;
   font-weight: 'bold';
+  cursor: pointer;
 }
 a {
   color: inherit;
